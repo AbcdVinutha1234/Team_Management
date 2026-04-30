be5f9efb3f42d2c05d6ab2ddacd8cf3e3b807efa
Vinutha S <vinuthas142003@gmail.com>
2026-04-30 16:00:01 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-project.firebaseapp.com",
-  projectId: "placeholder-project",
-  storageBucket: "placeholder-project.appspot.com",
-  messagingSenderId: "placeholder-sender-id",
-  appId: "placeholder-app-id"
+  "projectId": "studio-9813417248-459b3",
+  "appId": "1:804173098508:web:60aba4fd4d194c9fb08829",
+  "apiKey": "AIzaSyB_BXLt8QF-6YutAXoZf_bHwsn3UX0QCAg",
+  "authDomain": "studio-9813417248-459b3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "804173098508"
 };
